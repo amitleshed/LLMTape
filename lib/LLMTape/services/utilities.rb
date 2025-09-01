5fe2ff606decc964719a04e80c143ceb4d64a098
Amit Leshed <amitleshed@icloud.com>
2025-09-01 10:02:26 -0400

    create file if cannot be found

--- lib/LLMTape/services/utilities.rb
+++ lib/LLMTape/services/utilities.rb
@@ -8,10 +8,21 @@ module LLMTape
       Digest::SHA256.hexdigest(description)[0..7]
     end
 
+    def create_file
+      fixture_file_path = File.join(
+        LLMTape.fixtures_directory_path,
+        "llm_tapes.yml"
+      )
+      FileUtils.mkdir_p(LLMTape.fixtures_directory_path) unless Dir.exist?(LLMTape.fixtures_directory_path)
+      FileUtils.touch(fixture_file_path) unless File.exist?(fixture_file_path)
+      fixture_file_path
+    end
+
     def self.find_tape(fixture_path, description)
       return nil unless File.exist?(fixture_path)
 
       fixture_path = File.join(LLMTape.fixtures_directory_path, "llm_tapes.yml")
+      create_file unless File.exist?(fixture_path)
       docs = YAML.load_stream(File.read(fixture_path)).compact
       docs = [YAML.safe_load(File.read(fixture_path))] if docs.empty?
       target = description.to_s
