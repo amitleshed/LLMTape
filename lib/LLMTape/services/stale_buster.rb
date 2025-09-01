2bc721f46885f2f8cf26416effc7eeb0741f5b68
Amit Leshed <amitleshed@icloud.com>
2025-09-01 10:04:53 -0400

    fixes

--- lib/LLMTape/services/stale_buster.rb
+++ lib/LLMTape/services/stale_buster.rb
@@ -16,9 +16,11 @@ module LLMTape
       def self.stale?(current_prompt:)
         return true unless file_exists?
         fixture      = LLMTape::Backpack.find_tape(fixture_path, @description)
+        return true unless fixture
         created_at   = fixture.dig("data", "metadata", :created_at)&.to_s
+        return true unless created_at
         created_time = Time.parse(created_at) rescue nil
-        return true unless fixture && created_at && created_time
+        return true unless created_time
       
         too_old      = (Time.now - created_time) > 30*24*60*60
         prompt_diff  = fixture.dig("data", "request", :prompt).to_s != current_prompt.to_s
